abf289d30c07f86e6a16d8b1f27fef0720b67dd6
孙娇 <P.ozzy@outlook.com>
2022-12-04 21:58:55 +0800

    修改地图缓存实现方式。

--- lib/entity/tianditu.dart
+++ lib/entity/tianditu.dart
@@ -11,69 +11,57 @@ class TianDiTu {
   static List<TileLayer> vecTile = [
     // 街道图
     TileLayer(
-      tileProvider: CacheTileProvider(
-        tileName: 'vec',
-        urlTemplate:
-            'https://t{s}.tianditu.gov.cn/vec_w/wmts?SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&LAYER=vec&STYLE=default&TILEMATRIXSET=w&FORMAT=tiles&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}&tk=$key',
-        userAgentPackageName: packageName,
-        subdomains: const ['0', '1', '2', '3', '4', '5', '6', '7'],
-      ),
+      tileProvider: CacheTileProvider('vec'),
+      urlTemplate:
+          'https://t{s}.tianditu.gov.cn/vec_w/wmts?SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&LAYER=vec&STYLE=default&TILEMATRIXSET=w&FORMAT=tiles&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}&tk=$key',
+      userAgentPackageName: packageName,
+      subdomains: const ['0', '1', '2', '3', '4', '5', '6', '7'],
       backgroundColor: Colors.transparent,
     ),
     TileLayer(
-      tileProvider: CacheTileProvider(
-        tileName: 'cva',
-        urlTemplate:
-            'https://t{s}.tianditu.gov.cn/cva_w/wmts?SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&LAYER=cva&STYLE=default&TILEMATRIXSET=w&FORMAT=tiles&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}&tk=$key',
-        userAgentPackageName: packageName,
-        subdomains: const ['0', '1', '2', '3', '4', '5', '6', '7'],
-      ),
+      tileProvider: CacheTileProvider('cva'),
+      urlTemplate:
+          'https://t{s}.tianditu.gov.cn/cva_w/wmts?SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&LAYER=cva&STYLE=default&TILEMATRIXSET=w&FORMAT=tiles&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}&tk=$key',
+      userAgentPackageName: packageName,
+      subdomains: const ['0', '1', '2', '3', '4', '5', '6', '7'],
       backgroundColor: Colors.transparent,
     ),
   ];
   static List<TileLayer> imgTile = [
     // 卫星图
     TileLayer(
-      tileProvider: CacheTileProvider(
-        tileName: 'img',
-        urlTemplate:
-            'https://t{s}.tianditu.gov.cn/img_w/wmts?SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&LAYER=img&STYLE=default&TILEMATRIXSET=w&FORMAT=tiles&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}&tk=$key',
-        userAgentPackageName: packageName,
-        subdomains: const ['0', '1', '2', '3', '4', '5', '6', '7'],
-      ),
+      tileProvider: CacheTileProvider('img'),
+      urlTemplate:
+          'https://t{s}.tianditu.gov.cn/img_w/wmts?SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&LAYER=img&STYLE=default&TILEMATRIXSET=w&FORMAT=tiles&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}&tk=$key',
+      userAgentPackageName: packageName,
+      subdomains: const ['0', '1', '2', '3', '4', '5', '6', '7'],
       backgroundColor: Colors.transparent,
     ),
     TileLayer(
-      tileProvider: CacheTileProvider(
-        tileName: 'cia',
-        urlTemplate:
-            'https://t{s}.tianditu.gov.cn/cia_w/wmts?SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&LAYER=cia&STYLE=default&TILEMATRIXSET=w&FORMAT=tiles&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}&tk=$key',
-        userAgentPackageName: packageName,
-        subdomains: const ['0', '1', '2', '3', '4', '5', '6', '7'],
-      ),
+      tileProvider: CacheTileProvider('cia'),
+      urlTemplate:
+          'https://t{s}.tianditu.gov.cn/cia_w/wmts?SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&LAYER=cia&STYLE=default&TILEMATRIXSET=w&FORMAT=tiles&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}&tk=$key',
+      userAgentPackageName: packageName,
+      subdomains: const ['0', '1', '2', '3', '4', '5', '6', '7'],
       backgroundColor: Colors.transparent,
     ),
   ];
   static List<TileLayer> terTile = [
     // 地形图
     TileLayer(
-      tileProvider: CacheTileProvider(
-        tileName: 'ter',
-        urlTemplate:
-            'https://t{s}.tianditu.gov.cn/ter_w/wmts?SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&LAYER=ter&STYLE=default&TILEMATRIXSET=w&FORMAT=tiles&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}&tk=$key',
-        userAgentPackageName: packageName,
-        subdomains: const ['0', '1', '2', '3', '4', '5', '6', '7'],
-      ),
+      tileProvider: CacheTileProvider('ter'),
+      urlTemplate:
+          'https://t{s}.tianditu.gov.cn/ter_w/wmts?SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&LAYER=ter&STYLE=default&TILEMATRIXSET=w&FORMAT=tiles&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}&tk=$key',
+      userAgentPackageName: packageName,
+      subdomains: const ['0', '1', '2', '3', '4', '5', '6', '7'],
       backgroundColor: Colors.transparent,
     ),
     TileLayer(
-      tileProvider: CacheTileProvider(
-        tileName: 'cta',
-        urlTemplate:
-            'https://t{s}.tianditu.gov.cn/cta_w/wmts?SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&LAYER=cta&STYLE=default&TILEMATRIXSET=w&FORMAT=tiles&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}&tk=$key',
-        userAgentPackageName: packageName,
-        subdomains: const ['0', '1', '2', '3', '4', '5', '6', '7'],
-      ),
+      tileProvider: CacheTileProvider('cta'),
+      urlTemplate:
+          'https://t{s}.tianditu.gov.cn/cta_w/wmts?SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&LAYER=cta&STYLE=default&TILEMATRIXSET=w&FORMAT=tiles&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}&tk=$key',
+      userAgentPackageName: packageName,
+      subdomains: const ['0', '1', '2', '3', '4', '5', '6', '7'],
       backgroundColor: Colors.transparent,
     ),
   ];
