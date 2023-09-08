2049eeaffa25c559462bdad18ae082475d757404
Sun Jiao <P.ozzy@outlook.com>
2023-09-08 20:00:34 +0800

    Revert "add isInChina method"
    This reverts commit 8a2b90ddd1e56dce698fde4586e32fdd88aabe35.

--- shared/example/package_example.dart
+++ shared/example/package_example.dart
@@ -1,12 +1,6 @@
 import 'package:shared/shared.dart';
-import 'package:shared/src/in_china_tester.dart';
-import 'dart:math' as math;
 
 void main() {
-  final start = DateTime.now().microsecondsSinceEpoch;
-  for (int i = 0 ; i < 100000 ; i++) {
-    print(isInChina(math.Random().nextDouble() * 60, 70 + math.Random().nextDouble() * 70));
-  }
-  final end = DateTime.now().microsecondsSinceEpoch;
-  print((end - start).toString());
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
 }
